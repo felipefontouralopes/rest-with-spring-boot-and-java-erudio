628c32fd955933b735f5b1bda1cb3476d921fa0e
Felipe <felipefontouratec@gmail.com>
2026-03-01 20:48:22 -0300

    Aula 06.2

--- 06_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 06_WorkingWithVerbs/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio.services;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -21,6 +23,28 @@ public class PersonServices {
         person.setFirstName("Leandro");
         person.setLastName("Costa");
         person.setAddress("Uberlândia - Minas Gerais - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<Person>();
+
+        //Mock para teste
+        for (int i=0; i<8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i){
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Brasil");
         person.setGender("Male");
         return person;
     }
