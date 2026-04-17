c3fadf8aa9faf0b63242ace3ee2a33fdb8505137
Felipe Lopes <felipefontouratec@gmail.com>
2026-04-16 21:25:46 -0300

    Swagger Padrão]

---
+++ 15_WorkingWithSwaggerOpenAPI_NoRESTful/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package br.com.erudio.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("REST API from 0 with Java, Spring Boot, Kubernetes and Docker")
+                        .version("v1")
+                        .description("REST API from 0 with Java, Spring Boot, Kubernetes and Docker")
+                        .termsOfService("https://swagger.io/terms/")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://springdoc.org")
+                        )
+                );
+    }
+
+}
